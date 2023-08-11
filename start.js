3ef985366adfb85f150dfe4c8d6fdbc005ae2e99
Shivam-wz <56469003+Shivam-wz@users.noreply.github.com>
2023-08-12 00:32:44 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,121 @@
+// GET REQUEST
+function getTodos() {
+axios.get(url: 'https://jsonplaceholder.typicode.com/todos?', {params: { _limit:5}})
+.then((res)=> showOutput(res))
+.catch((err)=>console.log(err))
+}
+
+// POST REQUEST
+function addTodo() {
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title : "New todo ",
+    completed : false
+  })
+  .then((res)=>  console.log(res))
+  .catch((err)=> console.log(err))
+}
+
+// PUT/PATCH REQUEST
+function updateTodo() {
+  console.log('PUT/PATCH Request');
+}
+
+// DELETE REQUEST
+function removeTodo() {
+  console.log('DELETE Request');
+}
+
+// SIMULTANEOUS DATA
+function getData() {
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos'),
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+  ])
+  .then(res => {
+    console.log(res[0])
+    console.log(res[1])
+    showOutput(res[1])
+  })
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
+  console.log('Custom Headers');
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
+  console.log('Transform Response');
+}
+
+// ERROR HANDLING
+function errorHandling() {
+  console.log('Error Handling');
+}
+
+// CANCEL TOKEN
+function cancelToken() {
+  console.log('Cancel Token');
+}
+
+// INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime}`
+    )
+    return config
+  },
+  error => {
+    return promise.reject(error)
+)
+// AXIOS INSTANCES
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+
+// Event listeners
+document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('post').addEventListener('click', addTodo);
+document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('delete').addEventListener('click', removeTodo);
+document.getElementById('sim').addEventListener('click', getData);
+document.getElementById('headers').addEventListener('click', customHeaders);
+document
+  .getElementById('transform')
+  .addEventListener('click', transformResponse);
+document.getElementById('error').addEventListener('click', errorHandling);
+document.getElementById('cancel').addEventListener('click', cancelToken);
